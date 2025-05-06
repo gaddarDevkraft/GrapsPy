a2e82e0f97ef9f8457218c44ca8a826d049e1de2
Gaddar Kr. Chaudhary <gaddarkumarchaudhary@devkraft.in>
2025-05-06 16:15:13 +0530

    test open api key

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from typing import Union
-from fastapi import FastAPI, status
+from dotenv import load_dotenv
+from fastapi import FastAPI, status, HTTPException
+from openai import OpenAI
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+client = OpenAI(api_key=os.environ.get("DEVKRAFT_OPENAI_API_KEY"))
 
-@app.get("/")
-def hello():
-    return {"status": "RAG operation"}
-
+@app.get("/chat")
+def get_chatbot_response(q: str):
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user", "content": q
+                }
+            ]
+        )
+        return {"response": completion.choices[0].message.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 # import uvicorn
 # if __name__ == "__main__":
